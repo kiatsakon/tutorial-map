ce417f1398a6794586f0e014927356a215e05529
kiatsakon <40081343+kiatsakon@users.noreply.github.com>
2022-06-22 16:12:33 +0700

    change file map

--- tilesets/WA_LOGO.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.6" name="HELLOIT-V1.0-PLAIN" tilewidth="64" tileheight="64" tilecount="49" columns="7">
- <image source="C:/Users/LEMEL/Desktop/HELLOIT-V1.0-PLAIN.png" width="450" height="450"/>
-</tileset>
--- tilesets/WA_LOGO_OFFICE.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.6" name="logo_office-removebg-preview" tilewidth="192" tileheight="32" tilecount="4" columns="2">
- <image source="logo_office-removebg-preview.png" width="393" height="64"/>
-</tileset>
