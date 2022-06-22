2ca8d69fe6a8d4daf843b20e62382760435ed6ca
kiatsakon <40081343+kiatsakon@users.noreply.github.com>
2022-06-22 15:53:35 +0700

    update map

---
+++ tilesets/dog.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.6" name="dog" tilewidth="1" tileheight="1" tilecount="262144" columns="512">
+ <image source="C:/Users/LEMEL/Downloads/dog.png" width="512" height="512"/>
+</tileset>
